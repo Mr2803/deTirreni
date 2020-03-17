1cae7164c5ff21b172f3b45ea569e5877f1d3c43
Mr2803 <rispoli.mar2803@gmail.com>
2020-03-17 22:21:59 +0100

    init

---
+++ public/js/app.js
@@ -0,0 +1,25 @@
+const navSlide = () => {
+    const burger = document.querySelector('.myBurger');
+    const nav = document.querySelector('.my_nav-links');
+    const navLinks = document.querySelectorAll('.my_nav-links li')
+    //toggle nav
+    burger.addEventListener('click', () =>{
+    nav.classList.toggle('nav-active');
+        //animate links
+    navLinks.forEach((link,index)=>{
+        if(link.style.animation){
+            link.style.animation = ""
+            
+            console.log(index / 7 + 0.5)
+        } else{
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.4}s`
+        }
+    });
+    //Burger animation
+    burger.classList.toggle('toggle');
+
+})
+    
+}
+
+    navSlide();
