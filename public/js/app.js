dc8810fcd7fe0bb5e57e3af2f2b374cf26d3a80f
Mr2803 <rispoli.mar2803@gmail.com>
2020-03-18 13:24:40 +0100

    add owl carousel

--- public/js/app.js
+++ public/js/app.js
@@ -23,3 +23,20 @@ const navSlide = () => {
 }
 
     navSlide();
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
