242eff26a61689f9954b79401f9f4410ccdbeaf6
Mr2803 <rispoli.mar2803@gmail.com>
2020-03-18 19:09:12 +0100

    add details , create many pages(basic) . Add js to fix some bug

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,29 @@
-const navSlide = () => {
+
+
+function init(){
+
+    shadow();
+    navSlide();
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+}
+
+// slider navbar
+    const navSlide = () => {
     const burger = document.querySelector('.myBurger');
     const nav = document.querySelector('.my_nav-links');
     const navLinks = document.querySelectorAll('.my_nav-links li')
@@ -22,21 +47,28 @@ const navSlide = () => {
     
 }
 
-    navSlide();
 
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
+const shadow = () => {
+    var cards = document.getElementsByClassName('card');
+    console.log(cards.length)
+    for (let index = 0; index < cards.length; index++) {
+        cards[index].onmouseenter = function(){
+            this.classList.add("shadow")
+        }
+        cards[index].onmouseleave = function(){
+            this.classList.remove("shadow")
         }
     }
-})
+        
+}
+
+$(window).scroll(function(){
+    $(this).scrollTop() !== 0 ? ($('.myScrollOnTop').removeClass("d-none")) : ($('.myScrollOnTop').addClass("d-none"))
+});
+$(".myScrollOnTop").on("click", function() {
+    $("html").animate({ scrollTop: 0 }, "slow");
+});
+
+
+
+$(document).ready(init)
